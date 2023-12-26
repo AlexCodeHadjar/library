c97aee1f4018091590415253c4d00ff48441d1c3
alastclock@mail.ru <alastclock@mail.ru>
2023-12-26 16:59:47 +0300

    Внес измения в название модели

---
+++ library/Data/Models/Bibliographicmaterial.cs
@@ -0,0 +1,11 @@
+﻿namespace library.Data.Models
+{
+    public class Bibliographicmaterial
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public  Author author { get; set; }
+        public Publisher publisher { get; set; }
+
+    }
+}
