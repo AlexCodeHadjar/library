d724f3f986310db85270e9369521f3d699e199b3
alastclock@mail.ru <alastclock@mail.ru>
2023-12-26 13:39:11 +0300

    Добавил в проект модели Author,Book,Publisher по технологии MVC для дальнейшей работы с Controllers.

---
+++ library/Data/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace library.Data.Models
+{
+    public class Author
+    {
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string contacts { get; set; }
+        public string information {  get; set; }
+
+
+    }
+}
---
+++ library/Data/Models/Publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace library.Data.Models
+{
+    public class Publisher
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string contacts { get; set; }
+        public string address {  get; set; }
+    }
+}
