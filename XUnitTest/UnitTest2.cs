94c933e8cf6e73aab722b3cbd3fff97247ee3b12
alastclock@mail.ru <alastclock@mail.ru>
2024-02-01 21:32:58 +0300

    Перенес файл с базой данных. Изменил строку подключения к базе банных

---
+++ XUnitTest/UnitTest2.cs
@@ -0,0 +1,17 @@
+namespace XUnitTest
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            Assert.True(true);
+    
+        }
+        [Fact]
+        public void SecondTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
